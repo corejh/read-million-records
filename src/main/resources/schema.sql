5ec679b02a014c81ec3d434a8a88f9952181dca5
Joey Hoang <mr.joseph.hoang@gmail.com>
2026-05-21 07:13:43 -0400

    Remove employee_records data insertion script from schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,13 +5,11 @@ CREATE TABLE employee_records (
     salary DECIMAL(10, 2) NOT NULL
 );
 
-INSERT INTO employee_records (id, first_name, last_name, salary)
-SELECT
-    LEVEL,
-    'FirstName_' || LEVEL,
-    'LastName_' || LEVEL,
-    DBMS_RANDOM.VALUE(50000, 150000)
-FROM dual
-CONNECT BY LEVEL <= 1000000;
-
-COMMIT;
+-- INSERT INTO employee_records (id, first_name, last_name, salary)
+-- SELECT 
+--     X, 
+--     'FirstName_' || X, 
+--     'LastName_' || X, 
+--     50000 + (RAND() * 100000)
+-- FROM SYSTEM_RANGE(1, 1000000);
+-- COMMIT;
